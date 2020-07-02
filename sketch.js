39875d74862da7c7047cae128ffb243395d0eece
pallavibiswas <bispyrrole@gmail.com>
2020-07-02 20:36:08 +0530

    Files Added

--- sketch.js
+++ sketch.js
@@ -7,16 +7,18 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var BG = "sprites/bg.png";
 
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBackgroundImage();
 }
 
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
+
     world = engine.world;
 
 
@@ -42,9 +44,12 @@ function setup(){
 
     //log6 = new Log(230,180,80, PI/2);
     slingshot = new SlingShot(bird.body,{x:200, y:50});
+
+    
 }
 
 function draw(){
+    if(backgroundImg)
     background(backgroundImg);
     Engine.update(engine);
     //strokeWeight(4);
@@ -85,4 +90,21 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+ async function getBackgroundImage(){
+    var response= await fetch("http://worldtimeapi.org/api/timezone/America/New_York");
+    var responseJSON= await response.json();
+    //console.log(responseJSON);
+    var datetime= responseJSON.datetime;
+    //console.log(datetime);
+    var hour=datetime.slice(11,13);
+    //console.log(hour);
+    if (hour >= 06 && hour <= 19){
+       BG = "sprites/bg.png";
+    }
+    else {
+        BG = "sprites/bg2.jpg";
+    }
+    backgroundImg = loadImage(BG);
 }
